bb9c67bedd7b17a053c94343189c84eab0941d80
Jon Christensen <jon.christensen@10up.com>
2021-03-29 16:55:25 -0500

    swup example

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,2 +1,20 @@
+import Swup from 'swup'
+import SwupA11yPlugin from '@swup/a11y-plugin'
+import SwupPreloadPlugin from '@swup/preload-plugin'
+import SwupProgressPlugin from '@swup/progress-plugin'
+import SwupScrollPlugin from '@swup/scroll-plugin'
+
 document.querySelector('html').classList.remove('no-js')
 document.querySelector('html').classList.add('js')
+
+// eslint-disable-next-line no-unused-vars
+const swup = new Swup({
+  animateHistoryBrowsing: true,
+  containers: ['#main', '#overlay'],
+  plugins: [
+    new SwupA11yPlugin(),
+    new SwupPreloadPlugin(),
+    new SwupProgressPlugin(),
+    new SwupScrollPlugin()
+  ]
+}) // only this line when included with script tag
