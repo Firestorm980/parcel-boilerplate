291635b3442732dbd6a5de53c53e9ad82da13502
Jon Christensen <jon@jmchristensendesign.com>
2021-04-07 22:15:16 -0500

    updates to use timeline, easings, fix transitions

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -7,28 +7,55 @@ import gsap from 'gsap'
 document.querySelector('html').classList.remove('no-js')
 document.querySelector('html').classList.add('js')
 
-const main = document.getElementById('main')
-
 const swup = new Swup({
-  containers: ['#main'],
+  containers: ['#main', '#overlay'],
   plugins: [
     new JsPlugin([
       {
         from: '(.*)',
         to: '(.*)',
         in: (next) => {
-          main.style.opacity = 0
-          gsap.to(main, {
+          const tl = gsap.timeline()
+
+          tl.fromTo(document.getElementById('overlay'), {
+            xPercent: 0,
+            autoAlpha: 1
+          }, {
+            xPercent: 100,
+            duration: 0.5,
+            ease: 'power3.in',
+            delay: 1
+          })
+          tl.fromTo(document.getElementById('main'), {
+            opacity: 0,
+            y: 100
+          }, {
             opacity: 1,
-            duration: 5,
-            onComplete: next
+            y: 0,
+            duration: 0.5,
+            onComplete: next,
+            ease: 'back'
           })
         },
         out: (next) => {
-          main.style.opacity = 1
-          gsap.to(main, {
+          const tl = gsap.timeline()
+
+          tl.fromTo(document.getElementById('main'), {
+            opacity: 1,
+            y: 0
+          }, {
             opacity: 0,
-            duration: 5,
+            y: 100,
+            duration: 0.5,
+            ease: 'power3.out'
+          })
+          tl.fromTo(document.getElementById('overlay'), {
+            xPercent: -100,
+            autoAlpha: 1
+          }, {
+            xPercent: 0,
+            duration: 0.5,
+            ease: 'power3.out',
             onComplete: next
           })
         }
