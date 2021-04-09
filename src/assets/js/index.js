572db9da8c0d3a7f431ef3e4fcce089c5979b061
Jon Christensen <jon.christensen@10up.com>
2021-04-09 10:26:25 -0500

    add history support back

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -12,6 +12,7 @@ document.querySelector('html').classList.remove('no-js')
 document.querySelector('html').classList.add('js')
 
 const swup = new Swup({
+  animateHistoryBrowsing: true,
   containers: ['#main', '#overlay'],
   plugins: [
     new JsPlugin([
