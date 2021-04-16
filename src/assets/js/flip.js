61313aec314c78a82e8c05a250396359221df3aa
Jon Christensen <jon.christensen@10up.com>
2021-04-16 11:17:15 -0500

    comments

--- src/assets/js/flip.js
+++ src/assets/js/flip.js
@@ -3,12 +3,19 @@ const getRect = (el) => {
 }
 
 /**
-* FLIP
-* F = First
-* L = Last
-* I = Invert
-* P = Play
-*/
+ * FLIP helper function.
+ * F = First
+ * L = Last
+ * I = Invert
+ * P = Play
+ *
+ * Source: https://codepen.io/team/keyframers/pen/RwabMbR
+ * By: Keyframers https://keyframe.rs/
+ *
+ * @param {Function} doSomething The function to run that changes the UI in some way so we have our before and after states.
+ * @param {Array} firstEls The first set of targets to get position for.
+ * @param {Array} getLastEls That last set of targets to get position for and apply variables to. Don't use if using the same element throughout. Optional.
+ */
 export default function flip (doSomething, firstEls, getLastEls = () => firstEls) {
   // First
   const firstRects = firstEls.map(getRect)
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -6,19 +6,28 @@ document.querySelector('html').classList.add('js')
 document.addEventListener('click', (event) => {
   const { target } = event
 
+  // Only match the image
   if (!target.matches('img')) {
     return
   }
 
+  // Get the parent to apply the class
   const div = target.parentElement
 
+  // For transitioning, we want the image to always be on top. This helps us keep the z-index managed.
+  // We don't need data-transitioning after it finishes, so only run once.
   target.addEventListener('transitionend', () => { delete target.dataset.transitioning }, { once: true })
 
+  // Do the flip!
   flip(() => {
+    // Set transitioning for z-index management.
     target.dataset.transitioning = true
 
+    // Scroll to the top of the page. Not required, but possibly needed for the next part of our test (page loading!)
     window.scrollTo({ top: 0, behavior: 'smooth' })
 
+    // Update the UI
+    // Note, we're updating the div here, but the image is the element that is manipulated in our script.
     if (div.classList.contains('js-click')) {
       div.classList.remove('js-click')
     } else {
